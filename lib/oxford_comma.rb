1f742dd6514c27fe714ced73284c1a09b6a4ceb2
Alexander Hulteng <alexanderhulteng@gmail.com>
2020-08-25 18:31:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
-  if array.length == 2
-          array.join(" and ")
-      else
-        array.join
-      end
-
+  #if array.length == 2
+        #  array.join(" and ")
+      #else
+      #  array.join
+      #end
+      return list.join(' and ') if list.size < 3
+        list[-1] = "and " + list[-1]
+        list.join(', ')
 end
